a25fcc799d083831c6fdc6bcbf93b7b0a935ed7c
alexnanrick <alex.nanrick@gmail.com>
2020-03-01 22:20:54 +0000

    Add Patient class

---
+++ src/main/java/com/accenture/tagtrainingspring/patient/Patient.java
@@ -0,0 +1,15 @@
+package com.accenture.tagtrainingspring.patient;
+
+import java.time.LocalDate;
+
+public class Patient {
+
+    private String name;
+
+    private int id;
+
+    private LocalDate dateOfBirth;
+
+    private Gender gender;
+
+}
