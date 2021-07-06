1e1938dc3ef8f20b1bdac4b40d93a06efe5a4557
Therese <thereseohanlon@hotmail.com>
2021-07-06 10:24:17 +0100

    Morning of Jul 6th - running correctly

---
+++ src/main/java/com/accenture/tagtrainingspring/screening/Screening.java
@@ -0,0 +1,51 @@
+package com.accenture.tagtrainingspring.screening;
+
+import com.accenture.tagtrainingspring.patient.Patient;
+
+import java.time.LocalDate;
+
+public class Screening {
+
+    private int screeningId;
+    private Patient patient;
+    private LocalDate dateOfScreening;
+    private boolean malignant;
+
+    public Screening(int screeningId, Patient patient, LocalDate dateOfScreening, boolean malignant) {
+        this.screeningId = screeningId;
+        this.patient = patient;
+        this.dateOfScreening = dateOfScreening;
+        this.malignant = malignant;
+    }
+        public int getScreeningId () {
+            return screeningId;
+        }
+
+        public void setScreeningId (int screeningId){
+            this.screeningId = screeningId;
+        }
+
+        public Patient getPatient () {
+            return patient;
+        }
+
+        public void setPatient (Patient patient){
+            this.patient = patient;
+        }
+
+        public LocalDate getDateOfScreening () {
+            return dateOfScreening;
+        }
+
+        public void setDateOfScreening (LocalDate dateOfScreening){
+            this.dateOfScreening = dateOfScreening;
+        }
+
+        public Boolean isMalignant () {
+            return malignant;
+        }
+
+        public void setMalignant (boolean malignant){
+            this.malignant = malignant;
+        }
+    }
