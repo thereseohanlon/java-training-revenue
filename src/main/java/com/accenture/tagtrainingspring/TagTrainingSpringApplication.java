a00508feb6270e481ad27c3b2b885c8c4458c364
Therese <thereseohanlon@hotmail.com>
2021-07-06 09:41:59 +0100

    Morning of Jul 6th - running correctly

--- src/main/java/com/accenture/tagtrainingspring/TagTrainingSpringApplication.java
+++ src/main/java/com/accenture/tagtrainingspring/TagTrainingSpringApplication.java
@@ -1,14 +1,25 @@
 package com.accenture.tagtrainingspring;
 
+import com.accenture.tagtrainingspring.patient.Gender;
+import com.accenture.tagtrainingspring.patient.Patient;
+import com.accenture.tagtrainingspring.screening.Screening;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.LocalDate;
 
 @SpringBootApplication
 public class TagTrainingSpringApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(TagTrainingSpringApplication.class, args);
 		printWelcomeMessage();
+
+		Patient patient = new Patient("Tom", 1, LocalDate.of(1990, 1, 1), Gender.MALE);
+		Screening screening = new Screening(1, patient, LocalDate.of(2020, 4, 1), false);
+
+		System.out.println("Patient: " + screening.getPatient().getName() + " has a malignant diagnosis of " + screening.isMalignant());
 	}
 
 	private static void printWelcomeMessage() {
